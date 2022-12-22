cd8ffa9011665adcf78693b1acd2bc00355223b2
geekabhinav007 <abhinav26january@gmail.com>
2022-12-23 00:36:37 +0530

    Added thread Creation code.

--- Multithreading/CreatingThread.java
+++ Multithreading/CreatingThread.java
@@ -1,3 +1,32 @@
+// Creatin thread by Extending Thread class.
+
+class MyThread extends Thread{
+    @Override
+    public void run(){
+        int i = 1000;
+        while(i>0){
+        System.out.println("I am Cooking.");
+        i--;
+        }
+    }
+}
+
+class MyThread2 extends Thread{
+    @Override
+    public void run(){
+        int i = 1000;
+        while(i>0){
+        System.out.println("I am Playiing Game.");
+        i--;
+        }
+    }
+}
 public class CreatingThread {
-    
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread2 t2 = new MyThread2();
+        t1.start();
+        t2.start();
+        
+    }
 }
---
+++ Multithreading/ThreadCreation.java
@@ -0,0 +1,14 @@
+class MyThread implements Runnable { // Implemanting using Runnable Interface.
+    public void run() {
+        System.out.println("Thanks, I am a Thread.");
+    }
+}
+
+public class ThreadCreation {
+    public static void main(String[] args) {
+        
+    MyThread a1 = new MyThread();
+    Thread t1 = new Thread(a1);
+    t1.start();
+    }
+}
