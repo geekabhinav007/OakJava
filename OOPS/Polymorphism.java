9c13b6c1c4ec713fb657b7c54c789be8bd04d900
geekabhinav007 <abhinav26january@gmail.com>
2022-12-24 18:07:37 +0530

    polymorphism

--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -1,5 +1,33 @@
-package OOPS;
+class poly {
+    // Method Overloading
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public float add(float a, float b) {     // Method Overloading
+
+        return a + b;
+    }
+    public float add(float a, float b, float c) {    // Method Overloading
+
+        return a + b + c;
+    }
+    public float add(int a, float b) {     // Method Overloading
+
+        return a + b;
+    }
+    public int add(int a, int b, int c) {     // Method Overloading
+
+        return a + b + c;
+    }
+}
 
 public class Polymorphism {
-    
+public static void main(String[] args) {
+    poly obj = new poly();
+    System.out.println(obj.add(1,2));
+    System.out.println(obj.add(1,2,3));
+    System.out.println(obj.add(1.7f,2.6f));
+    System.out.println(obj.add(1.9f,2.5f,4.6f));
+    System.out.println(obj.add(1.9f,2));
+}
 }
