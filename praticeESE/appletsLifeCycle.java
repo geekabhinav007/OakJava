fdc31b80c755b0aab5792497602575584ad66042
geekabhinav007 <abhinav26january@gmail.com>
2022-12-26 01:00:16 +0530

    added applet life cycle

---
+++ praticeESE/appletsLifeCycle.java
@@ -0,0 +1,22 @@
+import java.applet.Applet;
+
+public class appletsLifeCycle extends Applet {
+    public void init() {
+        // initialized objects
+    }
+
+    public void start() {
+        // code to start the applet
+    }
+
+    public void paint(Graphics graphics) {
+        // draw the shapes
+    }
+
+    public void stop() {
+        // code to stop the applet
+    }
+
+    public void destroy() {  
+        // code to destroy the applet 
+}
