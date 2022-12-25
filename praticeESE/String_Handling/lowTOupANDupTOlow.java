17ccafa46200b79c52d023fd635a911b8ebfcff4
geekabhinav007 <abhinav26january@gmail.com>
2022-12-26 03:42:11 +0530

    some questions

---
+++ praticeESE/String_Handling/lowTOupANDupTOlow.java
@@ -0,0 +1,16 @@
+
+public class lowTOupANDupTOlow {
+    public static void main(String[] args) {
+
+        String lowerString = "abhinav is good boy";
+        String upperString = "I AM ABHINAV";
+
+        // Method-1
+        System.out.println(lowerString.toUpperCase());
+        System.out.println(upperString.toLowerCase());
+
+        // Method-2
+        
+
+    }
+}
