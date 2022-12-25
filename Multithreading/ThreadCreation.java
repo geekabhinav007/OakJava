1440781131dd69469b845ef3fa7e336be0f82b27
geekabhinav007 <abhinav26january@gmail.com>
2022-12-25 20:32:20 +0530

    Images fixed

--- Multithreading/ThreadCreation.java
+++ Multithreading/ThreadCreation.java
@@ -1,12 +1,12 @@
-class MyThread implements Runnable { // Implemanting using Runnable Interface.
+class MyThread implements Runnable {
+     // Implemanting using Runnable Interface.
     public void run() {
         System.out.println("Thanks, I am a Thread.");
     }
 }
 
 public class ThreadCreation {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {    
     MyThread a1 = new MyThread();
     Thread t1 = new Thread(a1);
     t1.start();
