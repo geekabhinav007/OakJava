b4bdeb9efd32ef59e3ecb4772f29e38f5f458585
geekabhinav007 <abhinav26january@gmail.com>
2022-12-25 23:56:10 +0530

    added q 19

--- File_Handling/Create.java
+++ File_Handling/Create.java
@@ -1,17 +1,29 @@
 
 
 import java.io.File;
+import java.io.IOException;
 
 public class Create {
-    public static void main(String[] args) {
+    // public static void main(String[] args) throws IOException {
+    //     File newFile = new File("Myfile2.txt");
+        
+    //     newFile.createNewFile();
+    //     System.out.println("File Created");
+        
+       
+    // }
+    public static void main(String[] args)  {
         File newFile = new File("Myfile2.txt");
-        try{
-        newFile.createNewFile();
-        System.out.println("File Created");
-        }
-        catch(Exception e){
+        
+        try {
+            newFile.createNewFile();
+        } catch (Exception e) {
+            
             System.out.println(e);
         }
+        System.out.println("File Created");
+        
+       
     }
     
 }
